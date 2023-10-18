7a10461730b93edd317dfead743b2de042cbb1ac
mkasiam <kawsarahmmed10001@gmail.com>
2023-10-18 10:03:49 +0600

    navbar , home and auth provider has been created

---
+++ src/components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">I am a nav bar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/layout/Root/Root";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./components/providers/AuthProvider/AuthProvider";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
