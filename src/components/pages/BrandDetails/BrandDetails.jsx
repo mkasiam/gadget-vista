9f5e9181fe0076b85687251d623c39232e0e4356
mkasiam <kawsarahmmed10001@gmail.com>
2023-10-20 21:41:39 +0600

    brand new json added with slider and good looking images

--- src/components/pages/BrandDetails/BrandDetails.jsx
+++ src/components/pages/BrandDetails/BrandDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import BrandProducts from "../BrandProducts/BrandProducts";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 
 const BrandDetails = () => {
   const [brandProducts, setBrandProducts] = useState([]);
@@ -8,7 +9,7 @@ const BrandDetails = () => {
   const brands = useLoaderData();
   //   desired brands is coming from the local json
   const desiredBrand = brands.find((details) => details.brand === brand);
-  const { id, image } = desiredBrand;
+  const { id, image, banner1, banner2, banner3 } = desiredBrand;
   useEffect(() => {
     fetch("https://gadget-vista-server.vercel.app/products")
       .then((res) => res.json())
@@ -17,12 +18,50 @@ const BrandDetails = () => {
   const desiredBrandProducts = brandProducts.filter(
     (cards) => cards.brand.toLowerCase() === brand.toLowerCase()
   );
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = [banner1, banner2, banner3];
+
+  const slide = (direction) => {
+    const newSlide = currentSlide + direction;
+
+    if (newSlide >= 0 && newSlide < images.length) {
+      setCurrentSlide(newSlide);
+    }
+  };
 
   return (
     <>
-      {/* Allocated Space for Banner  */}
-      <div>
-        <img src={image} alt="" />
+      {/* main div to show the banner content  */}
+      <div className="relative w-full h-64 lg:h-screen">
+        <div className="overflow-hidden w-full h-full">
+          <div
+            className="flex transition-transform duration-300 ease-in-out"
+            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+          >
+            {images.map((image, index) => (
+              <div key={index} className="w-full h-full flex-shrink-0">
+                <img
+                  src={image}
+                  alt={`Banner ${index + 1}`}
+                  className="w-full h-full object-center object-cover"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className="absolute top-1/2 left-4 transform -translate-y-1/2 text-white bg-blue-500 p-2 rounded-full cursor-pointer"
+          onClick={() => slide(-1)}
+        >
+          <FaAngleDoubleLeft></FaAngleDoubleLeft>
+        </button>
+        <button
+          className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white bg-blue-500 p-2 rounded-full cursor-pointer"
+          onClick={() => slide(1)}
+        >
+          <FaAngleDoubleRight></FaAngleDoubleRight>
+        </button>
       </div>
       {desiredBrandProducts.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 my-6">
